9e00539be4eff040a87c1dc49c1b85ad858008cc
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-08-27 11:22:15 +0200

    Ajout de styles sur la page d'accueil

---
+++ client/src/App.jsx
@@ -0,0 +1,77 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/06/YouTube-Thumb-Text-2-3.png"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <p className="info">
+            <a href="" className="author">Juju</a>
+            <time>2023-01-08 15:30</time>
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+            consectetur. At numquam aspernatur amet fuga?
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/06/YouTube-Thumb-Text-2-3.png"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <p className="info">
+            <a href="" className="author">Juju</a>
+            <time>2023-01-08 15:30</time>
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+            consectetur. At numquam aspernatur amet fuga?
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/06/YouTube-Thumb-Text-2-3.png"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <p className="info">
+            <a href="" className="author">Juju</a>
+            <time>2023-01-08 15:30</time>
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+            consectetur. At numquam aspernatur amet fuga?
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
